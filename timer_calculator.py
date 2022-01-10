65fde1aea771391dbc6cf1578eadbdc4c55a80f6
dmnekipelov <dm.nekipelov@gmail.com>
2022-01-10 23:58:26 +0200

    ДЗ 5. Использование функций в графическом интерфейсе

--- timer_calculator.py
+++ timer_calculator.py
@@ -3,31 +3,85 @@ import random
 import time
 
 start_time = time.time()
-results_list = []
+results_list = list()
+wrong_clicks = 0
+total_clicks = 0
 
 window = tk.Tk()
 window.geometry("300x300")
 window.title("Check your reaction")
 label = tk.Label(
-    text="Timer Calculator",
+    text="Let's play",
     font=("Arial", 15),
-    foreground="black",  # Set the text color to black
-    background="gray",  # Set the background color to gray
+    foreground="black",
+    background="gray",
     width=100,
-    height=2
+    height=3
 )
+
+
+def start_game():
+    for c in window.children:
+        window.children[c].pack()
+    greeting.pack_forget()
+    start_button.pack_forget()
+    window.after(2000, color_update)
+
+
+greeting = tk.Label(
+    text="Press 'Start' to launch",
+    font=("Arial", 15),
+    foreground="black",
+    background="orange",
+    width=100,
+    height=3
+)
+start_button = tk.Button(
+    text="Start",
+    font=("Arial", 12),
+    bg="red",
+    fg="black",
+    width=10,
+    height=1,
+    command=start_game
+)
+greeting.pack()
+start_button.pack(pady=10)
 
 frame = tk.Frame(
     height=10
 )
 
 
-def send_message():
-    run_time = round(time.time() - start_time, 2)
-    results_list.append(run_time)
-    average_time = round(sum(results_list) / len(results_list), 2)
-    result.config(text=f"Time: {run_time}")
-    result_average.config(text=f"Average time: {average_time}")
+def wrong_clicks_counter():
+    global wrong_clicks, total_clicks
+    total_clicks += 1
+    if label.config("text")[-1] != "red":
+        wrong_clicks += 1
+    result_wrong_click.config(text=f"False clicks: {wrong_clicks} from {total_clicks}")  # display false clicks
+
+
+def time_counter():
+    if label.config("text")[-1] == "red":
+        run_time = round(time.time() - start_time, 2)
+        results_list.append(run_time)
+        average_time = round(sum(results_list) / len(results_list), 2)
+        result.config(text=f"Time: {run_time}s")  # display result
+        result_average.config(text=f"Average time: {average_time}s")  # display average result
+
+
+def button_state_switcher(val):
+    if val != button.config("state")[-1]:
+        if val == "disabled":
+            button.config(state=val, bg="gray80")
+        elif val == "normal":
+            button.config(state=val, bg="orange")
+
+
+def show_result():
+    time_counter()
+    wrong_clicks_counter()
+    button_state_switcher("disabled")
 
 
 button = tk.Button(
@@ -35,47 +89,65 @@ button = tk.Button(
     font=("Arial", 12),
     bg="orange",
     fg="black",
-    width=10,
-    height=1,
-    command=send_message
+    width=12,
+    height=2,
+    command=show_result
 )
 
 result = tk.Label(
-    text="Time: 0.0",
-    foreground="red",  # Set the text color to red
+    text="Time: 0.0s",
+    foreground="green",
     font=("Arial", 15)
 )
 
 result_average = tk.Label(
-    text="Average time: 0.0",
-    foreground="red",  # Set the text color to red
+    text="Average time: 0.0s",
+    foreground="green",
+    font=("Arial", 15)
+)
+
+result_wrong_click = tk.Label(
+    text="False clicks: 0 from 0",
+    foreground="red",
     font=("Arial", 15)
 )
 
 frame_2 = tk.Frame(
-    height=10
+    height=40
 )
 
-result_1 = tk.Label(
-    foreground="green",  # Set the text color to white
-    font=("Arial", 25)
+
+def clear_data():
+    global wrong_clicks, total_clicks
+    wrong_clicks = 0
+    total_clicks = 0
+    results_list.clear()
+    result.config(text=f"Time: 0.0s")
+    result_average.config(text=f"Average time: 0.0s")
+    result_wrong_click.config(text=f"False clicks: {wrong_clicks} from {total_clicks}")
+
+
+reset_button = tk.Button(
+    text="Reset",
+    font=("Arial", 12),
+    fg="black",
+    width=10,
+    height=1,
+    command=clear_data
 )
 
-for c in window.children:
-    print(c)
-    window.children[c].pack()
 
-
-def timer_update():
-    colors = ["red", "green", "blue", "yellow"]
-    value = random.randint(0, len(colors) - 1)
-    label.config(background=colors[value], text=colors[value])
-    window.after(2000, timer_update)
-    if label.config("background")[-1] == "red":
+def color_update():
+    colors = ["red", "green", "yellow", "blue"]
+    title_color = colors[random.randint(0, len(colors) - 1)]
+    random_time = random.randint(1000, 3000)
+    label.config(background=title_color, text=title_color)
+    button_state_switcher("normal")
+    label.after(random_time - 300, lambda: label.config(background="gray", text=""))
+    window.after(random_time, color_update)
+    if label.config("text")[-1] == "red":
         global start_time
         start_time = time.time()
 
-
-window.after(2000, timer_update)
 
 window.mainloop()
